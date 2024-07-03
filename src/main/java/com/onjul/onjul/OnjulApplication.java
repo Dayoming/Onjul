2738ec1e7a831c3e3741accc54eb475bdb47bcbc
Dayoming <jdayoung1204@naver.com>
2024-07-03 16:56:44 +0900

    🎉 initial commit

---
+++ src/main/java/com/onjul/onjul/OnjulApplication.java
@@ -0,0 +1,13 @@
+package com.onjul.onjul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnjulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnjulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onjul/onjul/OnjulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onjul.onjul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnjulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
