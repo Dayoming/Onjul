93333245df435c892d5fdabc76637c1e880a4175
Dayoming <jdayoung1204@naver.com>
2024-07-08 22:33:51 +0900

    test: Add item detail inquiry function test #1
    교환 게시판 상품 상세 조회 기능 테스트 추가
    
    related to: #1

--- src/test/java/com/onjul/onjul/controller/ItemExchangeControllerTest.java
+++ src/test/java/com/onjul/onjul/controller/ItemExchangeControllerTest.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.onjul.onjul.constant.ItemCategory;
 import com.onjul.onjul.constant.ItemSellStatus;
 import com.onjul.onjul.dto.ItemDto;
+import com.onjul.onjul.entity.Item;
 import com.onjul.onjul.repository.ItemRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @WebMvcTest(ItemExchangeController.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -51,5 +54,24 @@ public class ItemExchangeControllerTest {
                 .content(objectMapper.writeValueAsString(itemDto)))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/exchange/"))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
+
+    @Test
+    @DisplayName("교환 게시판 특정 상품 조회 테스트")
+    public void ItemTest() throws Exception {
+        ItemDto itemDto = new ItemDto(null, "테스트 상품", ItemCategory.ETC, "admin", "admin",
+                10000, 2, "테스트 상품 상세 설명", ItemSellStatus.SELL, LocalDateTime.now(), LocalDateTime.now());
+        Item item = itemDto.toEntity();
+        itemRepository.save(item);
+
+        // ItemRepository의 동작을 모킹
+        Mockito.when(itemRepository.findById(0L)).thenReturn(Optional.of(item));
+
+        // GET 요청 수행 및 검증
+        mock.perform(MockMvcRequestBuilders.get("/exchange/0")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("item"))
+                .andExpect(MockMvcResultMatchers.model().attribute("item", item));
     }
 }
