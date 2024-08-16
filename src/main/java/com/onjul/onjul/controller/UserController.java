957600394bfc9feb9acb7e8849ae884115cae1d7
Dayoming <jdayoung1204@naver.com>
2024-08-16 14:32:35 +0900

    feat: 중복 회원가입시 예외 처리
    사용자 ID, 혹은 이메일 주소가 이미 존재하는 경우 발생하는 DataIntegrityViolationException 처리
    
    related to: #2

--- src/main/java/com/onjul/onjul/controller/UserController.java
+++ src/main/java/com/onjul/onjul/controller/UserController.java
@@ -4,6 +4,7 @@ import com.onjul.onjul.dto.UserCreateDto;
 import com.onjul.onjul.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,9 +35,18 @@ public class UserController {
             return "user/signup_form";
         }
 
-        userService.create(userCreateDto.getUsername(), userCreateDto.getEmail(),
-                userCreateDto.getPassword1(), userCreateDto.getNickname());
-
+        try {
+            userService.create(userCreateDto.getUsername(), userCreateDto.getEmail(),
+                    userCreateDto.getPassword1(), userCreateDto.getNickname());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "user/signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "user/signup_form";
+        }
         return "redirect:/";
     }
 }
